3a640df628afdbc8c9a5ede924341b9cb6d78421
NghiaPD <nghiapd@kaopiz.com>
2023-07-24 10:52:11 +0700

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { ListProvince } from './Province';
-
+import * as argon from 'argon2';
 const prisma = new PrismaClient();
 async function main() {
+  const password = await argon.hash('123456')
   prisma.$transaction([
     prisma.province.deleteMany({}),
     prisma.province.createMany({
@@ -14,10 +15,10 @@ async function main() {
         name: 'ADMIN',
         phone: '079914332',
         email: 'nghiak57dl1@gmail.com',
-        password: '123456',
-        role: 1
-      }
-    })
+        password: password,
+        role: 1,
+      },
+    }),
   ]);
 }
 
